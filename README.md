2037ccc1d933e66ab945a7047d420d926fe2c7c6
Andrea Brandi <me@andreabrandi.com>
2025-01-01 18:42:46 +0100

    feat: generate bitcoin taproot wallet address
    Output includes: address, private key and mnemonic.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Bitcoin Taproot Wallet Address Generator
+
+Generate your Taproot wallet address for Bitcoin with [Node](https://nodejs.org/en/download) via command line.
+
+- No Bitcoin Core or any other wallet software required.
+- No internet connection required during the generation.
+
+## Usage
+
+```sh
+node '.\wallet,js'
+```
+
+## Example output
+
+```
+Bitcoin Wallet (2025-01-01, 18:02:01):
+-----
+Taproot Address: bc1pt4x4uwyukqgr3t40339f43fpgsdwhtxph8p9pguw2574kspgk9psmn4uwr
+Private Key (WIF): Kxpc7M3A4CNHHizjJggcZpsNfZHps5buP98ir8FRUCheoAd2tHi2
+Mnemonic: banana sort belt exile beef cram welcome settle primary ring bargain keen
+```
---
+++ wallet.js
@@ -0,0 +1,57 @@
+// Andrea Brandi 2025 (ISC)
+// Import required libraries
+import * as ecc from "tiny-secp256k1";
+import { BIP32Factory } from "bip32";
+import { generateMnemonic, mnemonicToSeedSync } from "bip39";
+import { initEccLib, payments } from "bitcoinjs-lib";
+
+// Initialize ECC library and BIP32 factory
+const bip32 = BIP32Factory(ecc);
+initEccLib(ecc);
+
+// First receiving address of first account
+const DERIVATION_PATH = `m/86'/0'/0'/0/0`;
+// Utility to convert a public key to x-only format
+const toXOnly = (
+  pubKey,
+) => (pubKey.length === 32 ? pubKey : pubKey.slice(1, 33));
+
+/**
+ * Generates a new Taproot wallet.
+ * @returns {Object} Wallet details including address, WIF, and mnemonic.
+ */
+function generateTaprootWallet() {
+  // Generate mnemonic and seed
+  const mnemonic = generateMnemonic();
+  const seed = mnemonicToSeedSync(mnemonic);
+
+  // Derive the root key and child key
+  const rootKey = bip32.fromSeed(seed);
+  const childNode = rootKey.derivePath(DERIVATION_PATH);
+
+  // Derive x-only public key and create Taproot address
+  const xOnlyPubKey = toXOnly(childNode.publicKey);
+  const { address } = payments.p2tr({ internalPubkey: xOnlyPubKey });
+
+  // Get current date and time
+  const now = new Date();
+  const creationDate = now.toISOString().split("T")[0];
+  const creationTime = now.toTimeString().split(" ")[0];
+
+  return {
+    date: `${creationDate}, ${creationTime}`,
+    address,
+    wif: childNode.toWIF(),
+    mnemonic,
+  };
+}
+
+// Generate the wallet and log the details
+const wallet = generateTaprootWallet();
+const outputHead = `\nBitcoin Wallet (${wallet.date}):\n-----`;
+const taprootAddress = `- Taproot Address: ${wallet.address}`;
+const walletWif = `- Private Key (WIF): ${wallet.wif}`;
+const walletMnemonic = `- Mnemonic: ${wallet.mnemonic}`;
+console.log(
+  `${outputHead}\n${taprootAddress}\n${walletWif}\n${walletMnemonic}\n`,
+);
